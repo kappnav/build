a10c0f4e16043c101543175b32d4df3f762599a3
Chris Vignola <cvignola@us.ibm.com>
2019-08-09 13:21:46 -0500

    initial load

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#*****************************************************************
+#*
+#* Copyright 2019 IBM Corporation
+#*
+#* Licensed under the Apache License, Version 2.0 (the "License");
+#* you may not use this file except in compliance with the License.
+#* You may obtain a copy of the License at
+
+#* http://www.apache.org/licenses/LICENSE-2.0
+#* Unless required by applicable law or agreed to in writing, software
+#* distributed under the License is distributed on an "AS IS" BASIS,
+#* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#* See the License for the specific language governing permissions and
+#* limitations under the License.
+#*
+#*****************************************************************
+
+cd ../init ; ./build.sh; cd -
+cd ../operator; ./build.sh; cd -
+cd ../ui; ./build.sh; cd - 
+cd ../apis; ./build.sh; cd - 
+cd ../controller; ./build.sh; cd -
