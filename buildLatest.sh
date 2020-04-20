cfb779d3b9edda8b6d223589d3a731c2c5d0464b
Roger Cundiff <rogerleecundiff@gmail.com>
2020-04-20 11:02:44 -0500

    Build only updated repos

--- buildLatest.sh
+++ buildLatest.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #*****************************************************************
 #*
 #* Copyright 2020 IBM Corporation
@@ -17,6 +16,17 @@
 #*
 #*****************************************************************
 # Perform a build, install, and test of the latest commit in each repository
+
+# function for converting repo name to image name
+getImageName() {
+  dirName=$1
+  if [ $dirName == 'inventory' ]; then
+    imageName=kappnav-inv
+  else
+    imageName=kappnav-$dirName
+  fi
+}
+
 arg=$1
 
 # make sure we're running in the build directory
@@ -26,38 +36,63 @@ if [ $(echo $PWD | awk '{ n=split($0,d,"/"); print d[n] }') != 'build' ]; then
   arg="--?"
 fi
 
+# print help if we need to
 if [ x$arg == x'--?' ] || [  x$arg == x'-?' ]; then
-	echo "Build the latest kAppNav, install it and run the tests."
-	echo ""
-	echo ""
-	echo "syntax:"
-	echo ""
-	echo "buildLatest.sh"
+  echo ""
+  echo "Build the latest kAppNav, install it and run the tests.  Note that the"
+  echo "repositories will be left in the master branch."
+  echo ""
+  echo ""
+  echo "syntax:"
+  echo ""
+  echo "buildLatest.sh"
   echo ""
   echo "This script uses the following environment variables."
-  echo "   $CLUSTER_URL  - the URL to your OpenShift cluster"
-  echo "   $CLUSTER_USER - the user name for logging into your cluster; default is"
-  echo "                   kubeadmin"
-  echo "   $CLUSTER_PWD  - the password for logging into your cluster"
-  echo "   $DOCKER_ORG   - the Docker org to publish to and pull from"
+  echo "   \$CLUSTER_URL       - the URL to your OpenShift cluster"
+  echo "   \$CLUSTER_USER      - the user name for logging into your cluster; default is"
+  echo "                        is kubeadmin"
+  echo "   \$CLUSTER_PWD       - the password for logging into your cluster"
+  echo "   \$CLUSTER_PLATFORM  - the platform type of your cluster; default is ocp"
+  echo "   \$DOCKER_ORG        - the Docker org to publish to and pull from"
   echo ""
   echo "   The script will prompt for values for any missing environments variables"
   echo "   that don't have a default"
-	exit 1
+  exit 1
 fi
 
 # clean up our docker registry so we have enough space
+# automatically answer 'y' to the prune prompt
 echo "pruning Docker registry of dangling images"
-# docker image prune
+echo "y\n" | docker image prune
 
-# make sure all of the repositories are up-to-date
+# make sure all of the repositories are up to date
+# then rebuild any images that are out of date
+builtSomething="false"
 . ./projectList.sh
 for p in $BUILD_PROJECTS; do
-  echo "build $p"
-    # cd ../$p ; git checkout master; git pull; cd -
+  echo ""
+  echo "***** build $p"
+  cd ../$p ; git checkout master; git pull
+  commit=$(git rev-parse HEAD)
+  getImageName $p
+  docker image inspect $imageName | grep $commit
+  if [ $? -eq 0 ]; then
+    echo "$imageName is already current."
+  else
+    echo "building $imageName"
+    ./build.sh
+    builtSomething="true"
+  fi
+  cd -
 done
 
-# build, publish images, install, and test
+# if nothing was built then there's nothing else to do
+if [ $builtSomething == "false" ]; then
+  echo "All repositories are up to date.  No need to install and test."
+  exit
+fi
+
+# publish images, install, and test
 if [ x$CLUSTER_URL != x ]; then
   url=$CLUSTER_URL
 else
@@ -73,11 +108,15 @@ if [ x$CLUSTER_PWD != x ]; then
 else
   read -p "Enter your cluster password: " url
 fi
+if [ x$CLUSTER_PLATFORM != x ]; then
+  platform=$CLUSTER_PLATFORM
+else
+  platform=ocp
+fi
 if [ x$DOCKER_ORG != x ]; then
   org=$DOCKER_ORG
 else
   read -p "Enter the Docker org to use: " org
 fi
 
-
-echo ./setupKAppNavTestEnv.sh $url $user $pwd ocp $org -p
+./setupKAppNavTestEnv.sh $url $user $pwd $platform $org -p
